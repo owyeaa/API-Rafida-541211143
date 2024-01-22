b175403d3aedaf03bab6959f5d26da64f8aad77b
unknown <Rafidah>
2024-01-22 11:25:18 +0700

    first commit

---
+++ router/jobsheet.js
@@ -0,0 +1,6 @@
+const jobsheetController = require('../controllers/jobsheet'); const router = require('express').Router();
+
+router.post('/one', jobsheetController.submitOne);
+router.post('/many', jobsheetController.submitMany);
+
+module.exports = router;
